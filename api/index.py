178373d4a43fc4d126cfe56c76a87229ff8b0282
Soluções Tecnológicas <123423577+JuniorBSDN@users.noreply.github.com>
2026-08-27 16:09:37 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -854,10 +854,11 @@ def listar_chamados_gestor(empresa_id: int, db=Depends(get_db)):
     cursor = db.cursor()
     cursor.execute("""
         SELECT id, resumo_problema, descricao, status, 
-               TO_CHAR(data_criacao, 'YYYY-MM-DD HH24:MI:SS') as data_criacao, tecnico_responsavel
+               TO_CHAR(data_criacao, 'DD/MM/YYYY HH24:MI') as data_criacao, 
+               tecnico_responsavel
         FROM chamados 
         WHERE empresa_id = %s 
-        ORDER BY data_criacao DESC;
+        ORDER BY id DESC;
     """, (empresa_id,))
     chamados = cursor.fetchall()
     cursor.close()
